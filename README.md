8c9b6896a439faefc304811c292d54d9b399f95b
Todd Anderson <bustardcelly@gmail.com>
2015-06-26 17:14:04 +0200

    move of required libs to another directory.

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ Built using **Java 6 JDK**
 
 To Run
 ---
-In [Red5Bee.java](https://github.com/infrared5/rtmpbees/blob/master/src/main/java/com/infrared5/rtmpbee/Red5Bee.java#L28) the host, port and application are passed to the *Bee* - change them to whatever you need to test with.
+In [Red5Bee.java](https://github.com/infrared5/rtmpbee/blob/master/src/main/java/com/infrared5/rtmpbee/Red5Bee.java#L28) the host, port and application are passed to the *Bee* - change them to whatever you need to test with.
 
 Issue(s)
 ---
 No runtime or compile-time exceptions are thrown, however, when run as **Java Application**, it goes through connection request and immediately closes.
 
-Please refer to the [LOG](https://github.com/infrared5/rtmpbees/blob/master/LOG.txt) for further details.
+Please refer to the [LOG](https://github.com/infrared5/rtmpbee/blob/master/LOG.txt) for further details.
--- src/main/java/com/infrared5/rtmpbee/Red5Bee.java
+++ src/main/java/com/infrared5/rtmpbee/Red5Bee.java
@@ -1,6 +1,7 @@
 package com.infrared5.rtmpbee;
 
 import java.io.Console;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -27,7 +28,56 @@ public class Red5Bee {
 	 */
 	public static void main( String[] args ) {
     	
-    	if(args.length < 5) {
+		String url;
+		int port;
+		String application;
+		String streamName;
+		int numBullets;
+		int timeout = 10;
+		
+		// 3 option for client specific attack.
+		if(args.length < 2) {
+			Console console = System.console();
+			console.printf("Incorrect number of args, please pass in the following: \n "
+					+ "\narg[0] = RTMP URL"
+					+ "\narg[1] = numBullets");
+			return;
+		}
+		else if(args.length >= 2 && args.length <= 3) {
+			
+			URI uri;
+			String protocol;
+			String path;
+			String[] paths;
+			try {
+				uri = new URI(args[0]);
+				protocol = uri.getScheme();
+				url = uri.getHost();
+				port = uri.getPort();
+				path = uri.getPath();
+				paths = path.split("/");
+				if(paths.length < 3) {
+					System.console().printf("Could not properly parse provided endpoint for RTMP: " + args[0] + ".");
+					return;
+				}
+				System.console().printf("protocol: " + protocol + ", url: " + url + ", port: " + port + ", paths: " + paths[1] + ", " + paths[2] + ".\n");
+				application = paths[1];
+				streamName = paths[2];
+			}
+			catch(Exception e) {
+				System.console().printf("Could not properly parse provided endpoint for RTMP: " + args[0] + ".");
+				e.printStackTrace();
+				return;
+			}
+			
+			numBullets = Integer.parseInt(args[1]);
+			if(args.length > 2) {
+				timeout = Integer.parseInt(args[2]);
+			}
+			
+		}
+		// 5 option arguments for origin attack.
+		else if(args.length < 5) {
     		Console console = System.console();
     		console.printf("Incorrect number of args, please pass in the following: \n  "
     				+ "\narg[0] = IP Address"
@@ -38,22 +88,32 @@ public class Red5Bee {
     				
     		return;
     	}
-    	
-    	// we can setup our object
-    	bee = new Red5Bee(args);
-    	bee.loadMachineGun();
+		else {
+			
+			url = args[0];
+			port = Integer.parseInt(args[1]);
+			application = args[2];
+			streamName = args[3];
+			numBullets = Integer.parseInt(args[4]);
+			if(args.length > 5) {
+				timeout = Integer.parseInt(args[5]);
+			}
+			
+		}
+		
+		bee = new Red5Bee(url, port, application, streamName, numBullets, timeout);
+		bee.loadMachineGun();
     	bee.fireMachineGun();
+    	
     }
 	
-    public Red5Bee(String[] args) {
-		url = args[0];
-		port = Integer.parseInt(args[1]);
-		application = args[2];
-		streamName = args[3];
-		numBullets = Integer.parseInt(args[4]);
-		if(args.length > 5) {
-			timeout = Integer.parseInt(args[5]);
-		}
+    public Red5Bee(String url, int port, String application, String streamName, int numBullets, int timeout) {
+		this.url = url;
+		this.port = port;
+		this.application = application;
+		this.streamName = streamName;
+		this.numBullets = numBullets;
+		this.timeout = timeout;
 	}
 
     @SuppressWarnings("rawtypes")
@@ -80,4 +140,5 @@ public class Red5Bee {
 			machineGun.put(i, new Bullet(url, port, application, streamName, timeout));
 		}
 	}
+	
 }
